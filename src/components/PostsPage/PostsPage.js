123f1d83ad4e514dd1827d3c6976ed3eb5155b7f
Stanislav Kelemen <kelemen.work@gmail.com>
2021-04-02 03:19:48 +0300

    add endpoints constants

--- src/components/PostsPage/PostsPage.js
+++ src/components/PostsPage/PostsPage.js
@@ -3,62 +3,63 @@ import Modal from 'react-modal';
 import Button from '@material-ui/core/Button';
 import Pagination from '../Pagination';
 
+import { POSTS_URL } from '../../constants/endpoints';
+
 import PostItem from './PostItem';
 import AddPost from '../AddPost';
 import './PostsPage.scss';
- 
+
 const customStyles = {
-  content : {
-    top                   : '50%',
-    left                  : '50%',
-    right                 : 'auto',
-    bottom                : 'auto',
-    marginRight           : '-50%',
-    transform             : 'translate(-50%, -50%)',
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
     width: '500px',
     height: '500px'
   }
 };
 
-// const sortQuery = '?sort=date&descending=true';
-const URL = `https://vly41lw5kg.execute-api.us-east-1.amazonaws.com/dev/posts`;
 const numberOfPages = 2;
 
 const PostsPage = () => {
   const [posts, setPosts] = React.useState([]);
   const [currentPage, setCurrentPage] = React.useState(1);
-  const [modalIsOpen,setIsOpen] = React.useState(false);
+  const [modalIsOpen, setIsOpen] = React.useState(false);
 
   function openModal() {
     setIsOpen(true);
   }
- 
-  function closeModal(){
+
+  function closeModal() {
     setIsOpen(false);
   }
-  
-  React.useEffect(() => {
-      const fetchPosts = async (url) => {
-          const response = await fetch(`${url}?limit=${2}&page=${currentPage}`);
-          const responseJSON = await response.json();
 
-          setPosts(responseJSON);
-      };
-      fetchPosts(URL);
+  React.useEffect(() => {
+    const fetchPosts = async (url) => {
+      const response = await fetch(`${url}?limit=${2}&page=${currentPage}`);
+      const responseJSON = await response.json();
+
+      setPosts(responseJSON);
+    };
+
+    fetchPosts(POSTS_URL);
   }, [currentPage]);
 
-  const onDelete = (postId) => { 
+  const onDelete = (postId) => {
     const updatePosts = posts.filter(post => post.postId !== postId);
 
     setPosts(updatePosts);
   }
-  
+
   const renderDropdownItem = (post) => (
-      <PostItem 
-        key={post.postId} 
-        onDelete={onDelete} 
-        {...post} 
-      />
+    <PostItem
+      key={post.postId}
+      onDelete={onDelete}
+      {...post}
+    />
   );
 
   const onPostAdd = () => {
@@ -66,38 +67,39 @@ const PostsPage = () => {
       const response = await fetch(`${url}?limit=${2}&page=${currentPage}`);
       const responseJSON = await response.json();
 
-        setPosts(responseJSON);
+      setPosts(responseJSON);
     };
-    fetchPosts(URL);
+
+    fetchPosts(POSTS_URL);
   }
 
   return (
-      <div className="container">
-          {posts.map(renderDropdownItem)}
-          <div>
-            
-            <Button className="addPostButton" onClick={openModal} color="primary" variant="contained">Add Post</Button>
-            <Pagination 
-              currentPage={currentPage}
-              numberOfPages={numberOfPages}
-              setCurrentPage={setCurrentPage}
-            />
-            <Modal
-              isOpen={modalIsOpen}
-              onRequestClose={closeModal}
-              style={customStyles}
-              contentLabel="Example Modal"
-              ariaHideApp={false}
-            >
-              <div className="modalHeader">
-                <Button onClick={closeModal} variant="contained" color="secondary">close</Button>
-              </div>
+    <div className="container">
+      {posts.map(renderDropdownItem)}
+      <div>
 
-              <AddPost posts={posts} onPostAdd={onPostAdd} closeModal={closeModal}/>
-
-            </Modal>
+        <Button className="addPostButton" onClick={openModal} color="primary" variant="contained">Add Post</Button>
+        <Pagination
+          currentPage={currentPage}
+          numberOfPages={numberOfPages}
+          setCurrentPage={setCurrentPage}
+        />
+        <Modal
+          isOpen={modalIsOpen}
+          onRequestClose={closeModal}
+          style={customStyles}
+          contentLabel="Example Modal"
+          ariaHideApp={false}
+        >
+          <div className="modalHeader">
+            <Button onClick={closeModal} variant="contained" color="secondary">close</Button>
           </div>
+
+          <AddPost posts={posts} onPostAdd={onPostAdd} closeModal={closeModal} />
+
+        </Modal>
       </div>
+    </div>
   )
 }
 
---
+++ src/constants/endpoints.js
@@ -0,0 +1,1 @@
+export const POSTS_URL = 'https://vly41lw5kg.execute-api.us-east-1.amazonaws.com/dev/posts';
